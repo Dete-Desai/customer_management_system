932d5ba23e91474a3536a4c6a33a60c1ee5942ab
Dete-Desai <detedesai@gmail.com>
2022-04-27 16:47:09 +0300

    Initial Commit

---
+++ lapchan_crm/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Navbar />
+    <section class="section">
+      <router-view />
+    </section>
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/layout/Navbar.vue";
+export default {
+  name: "App",
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "../node_modules/bulma";
+</style>
